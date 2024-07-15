2a73f75f31c0c1d712f6dbd9ca9c7b34abcc4105
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-15 15:58:36 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+    #include <iostream>
+     
+    using namespace std;
+     
+    int main()
+    {
+        string word;
+        cin >> word;
+        if (word=="Ready?")
+        {
+            cout << "Yes" ;
+        }
+        else
+        {
+            cout << "No" ;
+        }
+        return 0;
+    }
